ac46fcc47996968401792cf62a7252f5f19d0970
Muhammad Farid <mfhosni94@gmail.com>
2020-12-27 19:35:19 +0200

    Second problem
    Write a program that print your name and your grade in a new line

---
+++ Exercise2/Exercise2.c
@@ -0,0 +1,15 @@
+/*
+ * Exercise2.c
+ *
+ *  Created on: Dec 27, 2020
+ *      Author: Muhammad Farid
+ */
+
+#include<stdio.h>
+
+int main(void){
+	char name[]="Muhammad Farid";							/*My Name*/
+	float grade = 95.4;										/*My grade*/
+	printf("My name is %s\nMy grade is %0.1f",name,grade);
+	return 0;
+}
