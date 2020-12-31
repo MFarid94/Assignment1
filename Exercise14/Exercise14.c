5ee15e6f352432d265e7ec32ec9bfea296518006
Muhammad Farid <mfhosni94@gmail.com>
2020-12-31 19:37:44 +0200

    Fourteenth problem
    Write a program to display English alphabets from A to Z

---
+++ Exercise14/Exercise14.c
@@ -0,0 +1,17 @@
+/*
+ * Exercise14.c
+ *
+ *  Created on: Dec 28, 2020
+ *      Author: Muhammad Farid
+ */
+
+#include<stdio.h>
+
+int main(void){
+	char i;
+	/* Loop for displaying alphabets */
+	for(i = 'A';i<='Z';i++){
+		printf("%c ",i);
+	}
+	return 0;
+}
