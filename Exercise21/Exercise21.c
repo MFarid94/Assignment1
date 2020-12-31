276ef33a1a86267e1e51bc18fdd81f9b6f63be07
Muhammad Farid <mfhosni94@gmail.com>
2020-12-31 20:00:05 +0200

    Twenty-first problem
    Write a program to display cross or X-shape using stars pattern

---
+++ Exercise21/Exercise21.c
@@ -0,0 +1,43 @@
+/*
+ * Exercise21.c
+ *
+ *  Created on: Dec 29, 2020
+ *      Author: Muhammad Farid
+ */
+
+#include<stdio.h>
+
+int main(void){
+	setvbuf(stdout , NULL , _IONBF , 0);						/* Disabling Buffer */
+	setvbuf(stderr , NULL , _IONBF , 0);
+
+	int i,j,rows;
+
+	    printf("Enter number of rows: ");
+	    scanf("%d",&rows);
+	    printf("\n");
+
+	    for(i=0;i<rows;i++)
+	    {
+	        if(i == rows/2){
+	        	continue;
+	        }
+	    	for(j=0;j<rows;j++)
+	        {
+	            if(j == rows/2){
+	            	continue;
+	            }
+	    		if( (j==i) || (j==rows-i-1) )
+	            {
+	                printf("*");
+	            }
+	            else
+	            {
+	                printf(" ");
+	            }
+	        }
+	        printf("\n");
+	    }
+	    return 0;
+
+}
